b3fb89685874f2e84a9a5f6c206f7818ab77696b
jorge <jorgearteaga8936@gmail.com>
2016-12-30 00:46:58 -0400

    compiled sass

--- gulpfile.js
+++ gulpfile.js
@@ -14,6 +14,10 @@ require('laravel-elixir-vue-2');
  */
 
 elixir((mix) => {
-    mix.sass('app.scss')
-       .webpack('app.js');
+    mix.sass([
+      'app.scss',
+      'styles.scss'
+     ])
+       .webpack('app.js')
+         .version('css/app.css');
 });
